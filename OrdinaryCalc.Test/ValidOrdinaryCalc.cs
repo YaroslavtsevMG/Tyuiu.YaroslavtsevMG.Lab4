4eb180e66356c596d0bd60fd52ad0addc2adddba
matveyyaros <matveyyaros0112@gmail.com>
2024-12-03 19:21:25 +0500

    .

--- OrdinaryCalc.Test/ValidOrdinaryCalc.cs
+++ OrdinaryCalc.Test/ValidOrdinaryCalc.cs
@@ -6,19 +6,22 @@ namespace OrdinaryCalc.Test
         [TestMethod]
         public void TestCalculateSumma()
         {
-
+            Assert.AreEqual(1, OrdinaryCalc.Lib.OrdinaryCalc.CalcSumma(1, 0));
         }
+        [TestMethod]
         public void TestCalculateDifference()
         {
-
+            Assert.AreEqual(10, OrdinaryCalc.Lib.OrdinaryCalc.CalcDifference(90, 80));
         }
+        [TestMethod]
         public void TestCalculateMultiplication()
         {
-
+            Assert.AreEqual(0, OrdinaryCalc.Lib.OrdinaryCalc.CalcMultiplication(1, 0));
         }
+        [TestMethod]
         public void TestCalculateQuotient()
         {
-
+            Assert.AreEqual(1000, OrdinaryCalc.Lib.OrdinaryCalc.CalcQuotient(1000000, 1000));
         }
     }
 }
